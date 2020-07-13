b1e328db62a5788b0c67932d039363f4f16bb9c6
Jesus Kauze <12-10273@usb.ve>
2020-07-12 21:34:00 -0400

    Init of my Personal Terminal Page

---
+++ README.md
@@ -0,0 +1,11 @@
+# Personal Simple Web
+Inspired by Terminal shell and The **M.A.C.** agrupation
+
+Its a simple but beatty single personal page, no Angular, no React, or any other framework, Just HTML, javascript and CSS (K.I.S.S philosophy: Keep It Simple S****)
+
+Enjoy! :smile:
+
+*JK*
+
+In M
+
---
+++ index.js
@@ -0,0 +1,65 @@
+// Speed => typerSpeed | isTag => Boolean | text => text to show
+var i = 0;
+var text, isTag;
+var speed = 4;
+// var activeAnswer = false;
+// var verify = true;
+var idhtml = 'console'
+
+// Getting the jkCV.txt
+var Typer = {
+    text: null,
+    file: "",
+    init: function () {
+        $.get(Typer.file, function (data) {
+            Typer.text = data;
+            Typer.text = Typer.text.slice(0, Typer.text.length - 1);
+        });
+    },
+}
+
+// set the file to read
+Typer.file = "jkCV.txt";
+Typer.init();
+
+
+// TypeWriter Effect
+function typeWriter() {
+    text = Typer.text.slice(0, ++i);
+    if (text === Typer.text) return;
+    document.getElementById(idhtml).innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return typeWriter();
+    setTimeout(typeWriter, speed);
+}
+
+// Prevent default form and show skills
+function mySubmitFunction(e) {
+    e.preventDefault();
+    i = 0
+    var texto = document.getElementById('technologies').value
+    if (texto == "y" || texto == "Y" || texto == "" || texto == "yes" || texto == "YES" || texto == "Yes") {
+        idhtml = 'skillsconsole'
+        Typer.file = "skills.txt";
+    } else if (texto == "n" || texto == "N" || texto == "no" || texto == "No" || texto == "NO") {
+        idhtml = 'skillsconsole'
+        Typer.file = "bye.txt";
+
+    } else {
+        idhtml = 'skillsconsole'
+        Typer.file = "error.txt";
+    }
+    document.getElementById('technologies').disabled = true
+    Typer.init();
+    typeWriter();
+    return false;
+}
+
+
+
+
+
