6cec2c6e7abde024c1bcf6a0190b191e6b3b9044
Jesus Kauze <12-10273@usb.ve>
2020-07-18 18:36:18 -0400

    more speed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Speed: typerSpeed | isTag: Boolean (to not take the html in txt) | text => text to read/show
 var i = 0;
 var text, isTag;
-var speed = 12;
+var speed = 8;
 var idhtml = 'console'
 
 // Typer get the .txt to read the content
